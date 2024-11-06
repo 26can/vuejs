b76c6e6707aed844c32c2827fff3769ff1f6d110
Daisy <yan.he@hyperin.com>
2024-11-06 12:35:01 +0200

    vue app with button component

---
+++ vue-project/src/components/ClickButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+defineProps({
+    text : {
+        type : String,
+        Required : true
+    },
+    count : {
+        type : Number,
+        Required: true
+    }
+})
+
+</script>
+
+<template>
+  <button @click="$emit('increase')">{{ text }} {{ count }}</button>
+</template>
